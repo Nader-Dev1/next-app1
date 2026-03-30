4657b6caf4ca8859495669d90cdd0a4a2838c0fd
Nader-Dev1 <nder0028@gmail.com>
2026-03-30 11:28:27 -0700

    edit color

--- src/components/organisms/Contact.tsx
+++ src/components/organisms/Contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="flex justify-center items-center bg-orange-500  gap-4">     
+    <div className="flex justify-center items-center bg-black-500  gap-4">     
     <h1>Contact Us</h1>
     </div>
   );
--- src/components/organisms/Services.tsx
+++ src/components/organisms/Services.tsx
@@ -1,6 +1,6 @@
 export default function Services() {
   return (
-    <div className="flex justify-center items-center bg-green-500  gap-4">     
+    <div className="flex justify-center items-center bg-white-500  gap-4">     
     <h1>Services</h1>
     </div>
   );
