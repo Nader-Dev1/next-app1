63036c166f5ad0001cc55eff572cb5c9128fb485
Nader-Dev1 <nder0028@gmail.com>
2026-03-30 11:13:14 -0700

    add services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 
 import React from "react";  
+import Services from "@/components/organisms/Services";
 import About from "@/components/organisms/About";
 export default function page() {
   return (
     <div>
      <About />
-
+     <Services />
     </div>
   );
 }
---
+++ src/components/organisms/Services.tsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <div className="flex justify-center items-center bg-green-500  gap-4">     
+    <h1>Services</h1>
+    </div>
+  );
+}
